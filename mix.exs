677421c90bb0ea24560bae0b5bf94dababf0c58c
Mark Cotner <mark.cotner@gmail.com>
2025-06-04 17:39:14 -0400

    tag release v0.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SnmpLib.MixProject do
   def project do
     [
       app: :snmp_lib,
-      version: "0.2.0",
+      version: "0.2.3",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       compilers: [:yecc] ++ Mix.compilers(),
